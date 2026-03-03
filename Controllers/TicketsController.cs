2d1f01bf38e5ad195cf439853112c6589249f246
justanutka <aniakaduka136@gmail.com>
2026-03-03 10:38:02 +0100

    Tickets added

---
+++ Controllers/TicketsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniDesk.Web.Controllers
+{
+    public class TicketsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
