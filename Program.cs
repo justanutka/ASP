10ab24793fcf2745521dc882000727d3d0ea8fbc
justanutka <aniakaduka136@gmail.com>
2026-05-14 16:53:44 +0200

    LAB 10 AMBITNE add endpoint filters and endpoint mapping(BLUE SECTION)

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using UniDesc.Web.Services;
-using UniDesc.Web.Models;
-using UniDesc.Web.DTOs;
 using UniDesc.Web.Exceptions;
+using UniDesc.Web.Endpoints;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UniDesc.Web;
@@ -93,64 +92,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-var ticketsApi = app.MapGroup("/api/v2/tickets")
-    .WithTags("Tickets v2");
-
-ticketsApi.MapGet("/", (ITicketService ticketService) =>
-{
-    var tickets = ticketService.GetAllTicketDtos();
-
-    return Results.Ok(tickets);
-})
-.WithName("GetV2Tickets")
-.WithOpenApi();
-
-ticketsApi.MapPost("/", (CreateTicketRequest request, ITicketService ticketService) =>
-{
-    try
-    {
-        var createdTicket = ticketService.CreateTicket(request);
-
-        return Results.Created($"/api/v2/tickets/{createdTicket.Id}", createdTicket);
-    }
-    catch (ArgumentException ex)
-    {
-        return Results.BadRequest(new
-        {
-            error = ex.Message
-        });
-    }
-})
-.WithName("CreateV2Ticket")
-.WithOpenApi();
-
-ticketsApi.MapPut("/{id:int}", (int id, UpdateTicketRequest request, ITicketService ticketService) =>
-{
-    try
-    {
-        var updatedTicket = ticketService.UpdateTicket(id, request);
-
-        return Results.Ok(updatedTicket);
-    }
-    catch (ArgumentException ex)
-    {
-        return Results.BadRequest(new
-        {
-            error = ex.Message
-        });
-    }
-})
-.WithName("UpdateV2Ticket")
-.WithOpenApi();
-
-ticketsApi.MapDelete("/{id:int}", (int id, ITicketService ticketService) =>
-{
-    ticketService.DeleteTicket(id);
-
-    return Results.NoContent();
-})
-.WithName("DeleteV2Ticket")
-.WithOpenApi();
+app.MapTicketEndpoints();
 
 app.Run();
 
